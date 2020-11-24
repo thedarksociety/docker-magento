a6a1e1c2f4bbe7ad6af46e2ca8111b2c5376df23
Rye Miller <rmiller2@lci1.com>
2020-11-23 17:46:40 -0700

    Addition of project creation from composer.

---
+++ lib/composer_create_project.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Environment installation from Composer
+
+[ -z "$1" ] && echo "Choose your installation version of Magento." && exit 1
+
+VERSION=$1
+
+composer create-project \
+    --no-install \
+    --repository-url=https://repo.magento.com/ \
+    --ignore-platform-reqs \
+    --prefer-dist \
+    magento/project-community-edition=$VERSION \
+    src
