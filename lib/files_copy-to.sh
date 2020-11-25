cef8d91128909f158e6e5bb5866c9f90e93f8a0d
Rye Miller <rmiller2@lci1.com>
2020-11-24 22:30:30 -0700

    Update location of Docker images. Addition of installation script. [WIP].

---
+++ lib/files_copy-to.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# File copy to the container from the host machine.
+
+CURRENT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+PROJECT_PATH=$(dirname "${PROJECT_PATH}}")
