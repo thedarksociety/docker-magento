fa439e0b6e5e094fefb7499ce5e7934d73088b9c
Rye Miller <rmiller2@lci1.com>
2020-11-24 17:32:54 -0700

    Update to exec cmd to allow multiple flags and live separate of project cmd.

--- lib/docker_exec-notty.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Running commands inside containers. Hmmmm....
-
-# docker-compose tty: true is the same? (docker exec -ti)
-#stdin_open too?
-
-docker-compose exec -T phpfpm "$@"
